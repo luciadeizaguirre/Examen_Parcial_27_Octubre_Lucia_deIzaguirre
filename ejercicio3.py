23929e79e79c4f77687b60c914dfcd98df354491
luciadeizaguirre <luciadeizaguirre@gmail.com>
2022-10-27 19:14:58 +0200

    ej3

---
+++ ejercicio3.py
@@ -0,0 +1,11 @@
+class Alumno():
+    def __init__(self, nombre, nota):
+        self.nombre=nombre
+        self.nota=nota
+        print(f"El alumno {self.nombre} ha obtenido un {self.nota} de 10 puntos")
+
+    def calificacion(self):
+        if self.nota >= 5:
+            print(f"El alumno {self.nombre} ha aprobado con un {self.nota}")
+        else:
+            print(f"El alumno {self.nombre} ha suspendido con un {self.nota}")
